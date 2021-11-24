d9e58bc0eee546bfd623c0472f966f7376c5e64c
YJH <duwlgud05@naver.com>
2021-11-24 17:47:32 +0900

    개새끼

---
+++ data.js
@@ -0,0 +1,14 @@
+var data = `
+        [{"id":1,"first_name":"Sibel","last_name":"Jobb","email":"sjobb0@sohu.com","gender":"Female","salary":14756},
+{"id":2,"first_name":"Corry","last_name":"Coker","email":"ccoker1@addthis.com","gender":"Male","salary":8278},
+{"id":3,"first_name":"Cherianne","last_name":"Beams","email":"cbeams2@drupal.org","gender":"Male","salary":6662},
+{"id":4,"first_name":"Alisha","last_name":"Sinton","email":"asinton3@uiuc.edu","gender":"Male","salary":12623},
+{"id":5,"first_name":"Barbaraanne","last_name":"Hawker","email":"bhawker4@amazon.co.uk","gender":"Female","salary":14471},
+{"id":6,"first_name":"Lance","last_name":"Matczak","email":"lmatczak5@gov.uk","gender":"Female","salary":4909},
+{"id":7,"first_name":"Essy","last_name":"Worlidge","email":"eworlidge6@facebook.com","gender":"Male","salary":3389},
+{"id":8,"first_name":"Allie","last_name":"Lesley","email":"alesley7@intel.com","gender":"Male","salary":12275},
+{"id":9,"first_name":"Quinta","last_name":"De Benedictis","email":"qdebenedictis8@telegraph.co.uk","gender":"Female","salary":8535},
+{"id":10,"first_name":"Alec","last_name":"Mitchelmore","email":"amitchelmore9@ifeng.com","gender":"Male","salary":9294}]
+        `;
+
+var dataAry = JSON.parse(data);
